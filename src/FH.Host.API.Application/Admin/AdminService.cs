c0147aa682244fcab6dedf0bae1741211acf15f2
Mr.Fang <2875616188@qq.com>
2020-10-11 08:22:49 +0800

    删除 获取当前客户端IP服务

--- src/FH.Host.API.Application/Admin/AdminService.cs
+++ src/FH.Host.API.Application/Admin/AdminService.cs
@@ -1,6 +1,4 @@
 ﻿using FH.Host.API.Infrastructure.Authorization.Jwt;
-using FH.Host.API.Infrastructure.Http.Client;
-using FH.Host.API.Infrastructure.Http.Client.Dto;
 using FH.Host.API.Infrastructure.Model;
 using FH.Host.API.Infrastructure.SqlSugar;
 using FH.Host.API.Model.DefaultEntity;
@@ -29,16 +27,6 @@ namespace FH.Host.API.Application.Admin
         }
 
         /// <summary>
-        /// 查询所有Admin
-        /// </summary>
-        /// <returns></returns>
-        [HttpGet("SellectAll")]
-        public async Task<List<FH_Admin>> SellectAll()
-        {
-            return await _context.GetAll();
-        }
-
-        /// <summary>
         /// 管理员登录
         /// </summary>
         /// <param name="adminName">管理员账号</param>
@@ -64,18 +52,13 @@ namespace FH.Host.API.Application.Admin
         }
 
         /// <summary>
-        /// 获取客户端IP地址
+        /// 查询所有Admin
         /// </summary>
         /// <returns></returns>
-        [HttpGet("GetClientIP")]
-        [AllowAnonymous]
-        public ClientAccessIPResultDto GetClientIP()
+        [HttpGet("SellectAll")]
+        public async Task<List<FH_Admin>> SellectAll()
         {
-            /*
-             * 原有思路，本想自己写IP地址获取方法，发现自己写的获取到的是0.0.0.0，
-             * 所以直接采用免费的 搜狐IP 的接口
-             */
-            return ClientAccessIPService.GetClientAccessIP();
+            return await _context.GetAll();
         }
     }
 }
